4eddc350d69f4426f220e04858955fe3bec8a856
Pushkar S <spushkar0071234@gmail.com>
2024-11-03 23:28:00 +0530

    added necessary spaces in home page

--- app/page.tsx
+++ app/page.tsx
@@ -40,11 +40,11 @@ export default function Home() {
         }}
       >
         <p style={{ fontSize: "3.2rem", lineHeight: "1.6", fontWeight: "500" }}>
-  <span style={{ color: "#FFD700", fontWeight: "700" }}>Classrooms, a place of learning.</span> <br />
+  <span style={{ color: "#FFD700", fontWeight: "700" }}>Classroom, a place of learning.</span> <br />
   
   <span style={{ fontSize: "2.8rem", fontWeight: "400" }}>
     But much of the time is wasted in 
-    <span style={{ fontStyle: "italic", color: "#FF6347" }}>updating attendance</span>, a&nbsp;
+    <span style={{ fontStyle: "italic", color: "#FF6347" }}>&nbsp;updating attendance</span>, a&nbsp;
     <span style={{ fontWeight: "bold", color: "#FF4500" }}>necessary</span> yet&nbsp;
     <span style={{ textDecoration: "underline", color: "#FF4500" }}>unwanted process</span>.
   </span> <br />
@@ -54,7 +54,7 @@ export default function Home() {
   </span> <br />
   
   <span style={{ fontSize: "2.4rem", fontWeight: "400", color: "#00CED1" }}>
-    With our <span style={{ fontWeight: "bold", color: "#1E90FF" }}>High-Precision</span> 
+    With our <span style={{ fontWeight: "bold", color: "#1E90FF" }}>High-Precision&nbsp;</span> 
     <span style={{ fontWeight: "bold", fontStyle: "italic", color: "#1E90FF" }}>location-based attendance system</span>.
   </span>
 </p>
